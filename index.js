7657999bcba0bc134d4b86b73924e892eaa94737
SUPRAZY-DEV <60803887+SUPRAZY-DEV@users.noreply.github.com>
2020-11-01 14:09:25 +0100

    📮 Adding comments

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const Server = require("./src/BaseBot");
 const bot = new Server({ partials: ['MESSAGE', 'REACTION']});
 
+/**
+ * @async 
+ * @event
+ * @commands
+ */
 ( async () => {
     bot._start()
 })()
